0ee7a37b4b90d5592b9433fc0f69ddd50e2f008d
Sergii <kryvkosergii@gmail.com>
2024-08-18 20:09:48 +0300

    HW4

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,54 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import axios from "axios";
+
+let raiseEmptySearchCriteria = () => {
+    iziToast.show({
+        message: `Fill search field`,
+        position: "topRight",
+        backgroundColor: 'rgb(250,128,114)',
+        messageColor: 'rgb(255,255,255)'
+    });
+}
+
+let raiseEmptyResponse = () => {
+    iziToast.show({
+        message: `Sorry, there are no images matching your search query. Please try again!`,
+        position: "topRight",
+        backgroundColor: 'rgb(250,128,114)',
+        messageColor: 'rgb(255,255,255)'
+    });
+}
+
+export async function loadData(searchText, callbackOnSuccess, callbackOnError, callOnFinally, currentPage = 1) {
+    if (searchText === null || searchText === "") {
+        raiseEmptySearchCriteria();
+        callOnFinally();
+        return;
+    }
+
+    try {
+        let response = await axios.get('https://pixabay.com/api/?', {
+            params: {
+                key: '45339856-2e70ead6ce9cf82bdbbd89c7e',
+                q: searchText,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: true,
+                per_page: 15,
+                page: currentPage
+            }
+        });
+
+        if (response.data.hits.length > 0) {
+            callbackOnSuccess(response.data);
+        } else {
+            raiseEmptyResponse();
+        }
+
+    } catch (error) {
+        callbackOnError(error)
+    } finally {
+        callOnFinally();
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,65 @@
+import { loadData } from './js/pixabay-api'
+import { render } from './js/render-functions';
+
+const mainForm = document.querySelectorAll(".search")[0];
+let loader = document.querySelector("div.loader-panel");
+let loadMoreBtn = document.querySelector(".load-more");
+
+loader.style.display = "none";
+loadMoreBtn.style.display = "none";
+
+let currentPage = 1;
+
+function showOrHideLoadMoreButton(loaded) {
+    if (loaded.totalHits > currentPage * 15) {
+        loadMoreBtn.style.display = "block";
+    } else {
+        loadMoreBtn.style.display = "none";
+    }
+}
+
+function scrollPage() {
+    let element = document.querySelector(".result-item");
+    let elementHeight = element.getBoundingClientRect()['height'];
+    window.scrollBy({
+        top: elementHeight * 2,
+        left: 0,
+        behavior: "smooth",
+    });
+}
+
+mainForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    loader.style.display = "block";
+    const oldElements = document.querySelectorAll("li.result-item");
+    const searchQuery = mainForm.elements['search-query'].value;
+    if (oldElements && oldElements.length > 0) {
+        oldElements.forEach(element => element.remove());
+    }
+
+    currentPage = 1;
+
+    loadData(searchQuery,
+        response => {
+            render(response.hits);
+            showOrHideLoadMoreButton(response);
+        },
+        error => console.error(error),
+        () => loader.style.display = "none",
+        currentPage);
+});
+
+loadMoreBtn.addEventListener("click", (event) => {
+    currentPage += 1;
+    const searchQuery = mainForm.elements['search-query'].value;
+    loadData(searchQuery,
+        response => {
+            render(response.hits);
+            showOrHideLoadMoreButton(response);
+            scrollPage();
+        },
+        error => console.error(error),
+        () => loader.style.display = "none",
+        currentPage);
+})
+
